336d260ad7f5b52e185a1ae865f18fa4ffe9dde4
Zechospartan <zec12daok@hotmail.com>
2022-10-15 16:11:11 +1100

    Player mov and tileset
    Player movement on a collidable tile map.
    Ground checks(currently buggy still works on walls coz walls are on ground layer)

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playerMovement : MonoBehaviour
+{
+    
+    Rigidbody2D rb;
+    private BoxCollider2D coll;
+    public float speed;
+    public float jumpForce;
+    public Camera MainCamera;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+    }
+    private void Awake()
+    {
+         rb = this.GetComponent<Rigidbody2D>();
+    }
+    //fixed frame update based on editor
+    private void  Update()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(HorizontalInput * speed, rb.velocity.y);
+        if (Input.GetKey(KeyCode.Space) && isGrounded()) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+    //called after all other update functions
+    private void LateUpdate()
+    {
+        
+      
+    }
+    private bool isGrounded() 
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+}
