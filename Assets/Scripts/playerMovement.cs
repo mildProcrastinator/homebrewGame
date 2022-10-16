c946d04d8cbe6b25b31fac9f70ef9ce8f35fc72f
Zechospartan <zec12daok@hotmail.com>
2022-10-16 16:20:09 +1100

    movement and tile colliders
    Player movement fixes and tilemap collider fixes

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
     private BoxCollider2D coll;
     public float speed;
     public float jumpForce;
@@ -14,41 +15,52 @@ public class playerMovement : MonoBehaviour
     private Animator anim;
     [SerializeField] private LayerMask jumpableGround;
 
+    float HorizontalInput = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
     private void Awake()
     {
          rb = this.GetComponent<Rigidbody2D>();
     }
     //fixed frame update based on editor
-    private void  Update()
+    private void  FixedUpdate()
     {
-        float HorizontalInput = Input.GetAxis("Horizontal");
+        HorizontalInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(HorizontalInput * speed, rb.velocity.y);
         if (Input.GetKey(KeyCode.Space) && isGrounded()) 
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
         //Animation logic
         if (HorizontalInput > 0f)
         {
             anim.SetBool("running", true);
+            sprite.flipX = false;
         }
         else if (HorizontalInput < 0f)
         {
             anim.SetBool("running", true);
+            sprite.flipX = true;
         }
         else 
         {
             anim.SetBool("running", false);
         }
     }
+
     //called after all other update functions
     private void LateUpdate()
     {
