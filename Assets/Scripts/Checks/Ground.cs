c9bfaa2cf388114ea9e1ea849d171cbd67cf2fbb
Zechospartan <zec12daok@hotmail.com>
2022-10-21 17:09:38 +1100

    Move Script(Not Working)
    Finished off Ground script and added move script. Player still not moving

--- Assets/Scripts/Checks/Ground.cs
+++ Assets/Scripts/Checks/Ground.cs
@@ -7,24 +7,46 @@ public class Ground : MonoBehaviour
     private bool onGround;
     private float friction;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        EvaluateCollision(collision);
+        RetrievFriction(collision);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        
+        EvaluateCollision(collision);
+        RetrievFriction(collision);
     }
-
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        onGround = false;
+        friction = 0;
+    }
     private void EvaluateCollision(Collision2D collision)
     {
         for(int i=0; i < collision.contactCount; i++)
         {
             Vector2 normal = collision.GetContact(i).normal;
-            onGround = normal.y >= 0.9f;
+            onGround |= normal.y >= 0.9f;
         }
+    }
+    private void RetrievFriction(Collision2D collision) 
+    {
+        PhysicsMaterial2D material = collision.rigidbody.sharedMaterial;
+
+        friction = 0;
+        if (material != null) 
+        {
+            friction = material.friction;
+        }
+    }
+
+    public bool GetOnGround() 
+    {
+        return onGround;
+    }
+    public float GetFriction() 
+    {
+        return friction;
     }
 }
