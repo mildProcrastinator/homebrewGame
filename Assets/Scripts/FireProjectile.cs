7af48188abc77d6014b4d32271ae6fdaf5d0f5e0
mildProcrastinator <mfozan2@gmail.com>
2022-11-12 22:24:06 +1100

    Cutscene 1 scene COMPLETE

--- Assets/Scripts/FireProjectile.cs
+++ Assets/Scripts/FireProjectile.cs
@@ -28,7 +28,6 @@ public class FireProjectile : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.layer.ToString());
         if (collision.gameObject.layer.ToString().Equals("0"))
         {
             Destroy(this.gameObject);
