4504dcd5b917a16f849020bfc34e7421e0c98b6b
Zechospartan <zec12daok@hotmail.com>
2022-11-29 23:57:57 +1100

    Mob Trigger Boxes

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 using System;
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
     public Sound[] sounds;
     // Start is called before the first frame update
     void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else 
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -20,6 +31,11 @@ public class AudioManager : MonoBehaviour
     public void Play(string name) 
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) 
+        {
+            Debug.LogWarning("Sound: " + s.name + " is not in the Audio Manager or is spelt incorrectly!");
+            return;
+        }
         s.source.Play();
     } 
 }
